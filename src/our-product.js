fa3c9e10b1dc6ae383c284e797ee178c35dff878
NikNas1405 <103371039+NikNas1405@users.noreply.github.com>
2023-04-08 17:58:33 +0300

    08/04 оновлено js для секції our-product
    - оновлено js для секції our-product
    - оновлено css для секції our-product

--- src/our-product.js
+++ src/our-product.js
@@ -1,8 +1,26 @@
-var swiper = new Swiper('.swiper-product', {
-   slidesPerView: 'auto',
+const swiper = new Swiper('.swiper-product', {
+   direction: 'horizontal',
+   slidesPerView: '1',
    spaceBetween: 18,
    pagination: {
       el: '.swiper-pagination',
       clickable: true,
+   },
+   simulateTouch: true,
+   touchRatio: 2,
+   slideToClecedSlide: true,
+   freeMode: true,
+   a11y: {
+      enabled: true,
+   },
+
+   breakpoints: {
+      768: {
+         slidesPerView: 2.5,
+      },
+
+      1200: {
+         slidesPerView: 4,
+      },
    },
 });
