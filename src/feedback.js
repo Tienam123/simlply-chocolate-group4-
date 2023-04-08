ddbd2daa397ebdbdaa1fd3ec2f6d3550202759ea
TanyaMosk <tanya856227@gmail.com>
2023-04-08 14:40:19 +0200

    Modal feedback, added new file "feedback.js."

---
+++ src/feedback.js
@@ -0,0 +1,20 @@
+(() => {
+  const mobileMenu = document.querySelector('[data-modal]');
+  const openMenuBtn = document.querySelector('[data-modal-open]');
+  const closeMenuBtn = document.querySelector('[data-modal-close]');
+  const toggleMenu = () => {
+    const isMenuOpen =
+      openMenuBtn.getAttribute('aria-expanded') === 'true' || false;
+    openMenuBtn.setAttribute('aria-expanded', !isMenuOpen);
+    mobileMenu.classList.toggle("is-hidden");
+
+    const scrollLockMethod = !isMenuOpen
+      ? 'disableBodyScroll'
+      : 'enableBodyScroll';
+    bodyScrollLock[scrollLockMethod](document.body);
+  };
+
+  openMenuBtn.addEventListener('click', toggleMenu);
+  closeMenuBtn.addEventListener('click', toggleMenu);
+  
+})();
