31cdea4306fcdef294dad65728581be845280e82
OksanaShypik <kipishoks@gmail.com>
2023-04-09 09:45:47 +0200

    swiper

---
+++ src/review.js
@@ -0,0 +1,22 @@
+new Swiper('.swiper-container', {
+   direction: 'horizontal',
+   slidesPerView: '1',
+   // loop: true,
+   spaceBetween: 16,
+   grabCursor: true,
+   simulateTouch: true,
+   touchRatio: 2,
+   pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+   },
+   breakpoints: {
+      768: {
+         spaceBetween: 28,
+         slidesPerView: 2,
+      },
+      1200: {
+         slidesPerView: 3,
+      },
+   },
+});
