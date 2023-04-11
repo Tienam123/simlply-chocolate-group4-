b50fd2d3a780665db34334cd50938a94cbe5b03c
NavrA82 <124286244+NavrA82@users.noreply.github.com>
2023-04-12 00:12:16 +0300

    NavrA82-howtomade-non-video

--- src/video.js
+++
@@ -1,6 +0,0 @@
-  document.getElementById('button-id').addEventListener('click', function () {
-  document.getElementById('my-image').style.zIndex = 1;
-  document.getElementById('iframe-id').style.zIndex = 11;
-  document.getElementById('iframe-id').src ='https://www.youtube.com/embed/41UbWmXKWkc?autoplay=1';
-  document.getElementById('button-id').classList.add('hidden');
-  });
