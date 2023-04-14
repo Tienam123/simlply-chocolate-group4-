ebe33792f94b50128a605ac4b79ba7ae115b4829
NavrA82 <124286244+NavrA82@users.noreply.github.com>
2023-04-14 18:48:48 +0300

    NavrA82-Video-14-04-18-48

--- src/video.js
+++ src/video.js
@@ -1,44 +1,33 @@
-const image = document.getElementById('howtomade-image');
-const video = document.getElementById('howtomade-video-id');
-const playButton = document.getElementById('howtomade-video-button-id');
-let savedTime = 0;
+var video = document.getElementById('howtomade-video-id');
+var playButton = document.getElementById('howtomade-video-button-id');
+var image = document.getElementById('howtomade-image');
+let lastTime = 0;
 
 playButton.addEventListener('click', function () {
    video.play();
-   image.style.zIndex = 0;
-   playButton.classList.add('hidden');
-   video.style.zIndex = 1;
+      image.style.zIndex = 0;
+      playButton.classList.add('hidden');
 });
 
-video.addEventListener('ended', function () {
-   image.style.zIndex = 1;
-   playButton.classList.remove('hidden');
-   video.style.zIndex = 0;
+image.addEventListener('click', function () {
+   if (!video.paused) {
+      video.pause();
+   } else {
+      video.play();
+   }
 });
 
 video.addEventListener('click', function () {
    if (video.paused) {
       video.play();
    } else {
-      savedTime = video.currentTime;
+      lastTime = video.currentTime;
       video.pause();
    }
 });
 
-video.addEventListener('play', function () {
-   if (video.currentTime === 0 && savedTime) {
-      video.currentTime = savedTime;
-      savedTime = 0;
-   }
-});
-
-video.addEventListener('click', function () {
-   if (video.paused && video.currentTime !== video.duration) {
-      savedTime = video.currentTime;
-      video.play();
-      playButton.classList.remove('hidden');
-   } else if (!video.paused) {
-      video.pause();
-      playButton.classList.add('hidden');
+video.addEventListener('timeupdate', function () {
+   if (video.paused) {
+      video.currentTime = lastTime;
    }
 });
